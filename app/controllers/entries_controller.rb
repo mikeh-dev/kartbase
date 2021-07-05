9e8538a88040963ebbaf32073c8f65958cc1e349
mikeh-dev <mikeh112@hotmail.com>
2021-07-05 14:31:14 +0100

    new session now only duplicates needed data, fixed some controller name errors

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -14,7 +14,9 @@ class EntriesController < ApplicationController
   def new
     @entry = Entry.new
     @entry = Entry.last.dup
+    new_blanks
     @entry.time = Time.now.strftime("%k:%M")
+    @entry.date = Time.now.strftime("%d/%m/%Y")
   end
 
   # GET /entries/1/edit
@@ -64,7 +66,27 @@ class EntriesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def entry_params
-      params.require(:entry).permit(:track, :date, :time, :run, :gearing, :prepressure, :postpressure, :rearwidth, :frontwidth, :needleclip, :jet, :bestlap, :secondbestlap, :thirdbestlap, :airmix, :idle, :rimset, :tyre, :tyreset, :camber, :caster, :toe, :frontride, :rearride, :plug, :frontbar, :fuelload, :fuelmix, :condition, :chain, :axle, :enginehour, :tyreage, :waddingage, :gearoilage, :sessiontype, :mainchanges, :sessionnotes)
-      
+      params.require(:entry).permit(:track, :date, :time, :run, :gearing, :prepressure, :postpressure, :rearwidth, :frontwidth, :needleclip, :jet, :bestlap, :secondbestlap, :thirdbestlap, :airmix, :idle, :rimset, :tyre, :tyreset, :camber, :caster, :toe, :frontride, :rearride, :plug, :frontbar, :fuelload, :fuelmix, :condition, :chain, :axle, :endweight, :lapcount, :enginehour, :tyreage, :waddingage, :gearoilage, :sessiontype, :mainchanges, :sessionnotes, :besttoprevs, :bestbottomrevs, :secondbestlaptoprevs, :secondbestlapbottomrevs, :thirdbesttoprevs, :thirdbestbottomrevs, :highestrevs, :engine, :endfuel)
+    end
+
+    def new_blanks
+      @entry.bestlap = nil
+      @entry.secondbestlap = nil
+      @entry.thirdbestlap = nil
+      @entry.run += 1
+      @entry.mainchanges = nil
+      @entry.sessionnotes = nil
+      @entry.enginehour += 0.1
+      @entry.lapcount = nil
+      @entry.besttoprevs = nil
+      @entry.secondbestlaptoprevs = nil
+      @entry.thirdbesttoprevs = nil
+      @entry.bestbottomrevs = nil
+      @entry.secondbestlapbottomrevs = nil
+      @entry.thirdbestbottomrevs = nil
+      @entry.postpressure = nil
+      @entry.fuelload = nil
+      @entry.waddingage += 0.1
+      @entry.gearoilage += 0.1
     end
 end
