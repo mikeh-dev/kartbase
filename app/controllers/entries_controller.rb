c86c67405cfd945f61f3bf8ac4e1ad842389b47d
mikeh-dev <mikeh112@hotmail.com>
2021-07-06 14:43:55 +0100

    switched index view to sort in time order

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -3,7 +3,7 @@ class EntriesController < ApplicationController
 
   # GET /entries or /entries.json
   def index
-    @entries = Entry.all.order(date: :asc)
+    @entries = Entry.all.order(time: :asc)
   end
 
   # GET /entries/1 or /entries/1.json
